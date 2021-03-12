eafb8d6aecc365b1ba9e0e4ed4e874f36a3a6d60
JSgoddess <76064953+JSgoddess@users.noreply.github.com>
2021-03-12 13:31:51 -0700

    Add files via upload

---
+++ BankBalance.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BankBalance
+{
+public static void main(String[] args) 
+	{
+		//variables and constants 
+		double balance;
+		int response;
+		int year = 1;
+		final double INT_RATE = 0.03;
+		Scanner keyboard = new Scanner(System.in);
+
+		//input phase
+		System.out.print("Enter initial bank balance >> ");
+		balance = keyboard.nextDouble();
+	    System.out.println("Do you want to see next year's balance?");
+		System.out.print("Enter 1 for Yes, or any other Number for no >> ");
+		response = keyboard.nextInt();
+
+
+		// calculation and output phases
+		while(response == 1)
+		{
+			balance = balance + balance * INT_RATE;
+			System.out.println("After year " + year + " at" + INT_RATE + " interest rate, balance is $" + balance);
+			year = year + 1;
+			System.out.println("Do you want to see next year's balance?");
+			System.out.print("Enter 1 for Yes, or any other Number for No >> ");
+			response = keyboard.nextInt();
+		}
+
+		// final sign off message to indicate the program is exiting
+		System.out.println("Have a great day");
+	}
+	
+}
